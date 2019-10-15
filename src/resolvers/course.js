c997e9efb73f6686983b94fbe55e2d7c14f605ea
Joebbrian <job.bundabunda@gmail.com>
2019-10-15 21:58:01 +0200

    added input validation to course mutation

--- src/resolvers/course.js
+++ src/resolvers/course.js
@@ -1,59 +1,75 @@
-import { AuthenticationError } from 'apollo-server-express'
-import { Course } from '../models/courses'
-import { Unit } from '../models/unit'
-import { Topic } from '../models/topic'
+import { AuthenticationError } from "apollo-server-express";
+import { Course } from "../models/courses";
+import { Unit } from "../models/unit";
+import { Topic } from "../models/topic";
+import * as yup from "yup";
 
 const resolvers = {
   Query: {
     getCourses(root, args, { user }) {
-      return Course.find({})
-    },
+      return Course.find({});
+    }
   },
   Course: {
     // funny thing, you have access to the whole course
     units: course => {
-      return Unit.find({ courseId: course._id })
+      return Unit.find({ courseId: course._id });
     },
     topics: course => {
-      return Topic.find({ courseId: course._id })
-    },
+      return Topic.find({ courseId: course._id });
+    }
   },
   Mutation: {
-    addCourse(root, args, { user }) {
+    async addCourse(root, args, { user }) {
       if (!user) {
-        throw new AuthenticationError('you must be logged in to add a course')
+        throw new AuthenticationError("you must be logged in to add a course");
       }
-      let course = new Course()
-      course.name = args.name
-      course.createdAt = new Date()
-      course.createdBy = user._id
-      return course.save()
+      let validate = yup.object().shape({
+        name: yup.string().required()
+      });
+      await validate.validate(args).catch(function(err) {
+        throw new Error(err);
+      });
+
+      let course = new Course();
+      course.name = args.name;
+      course.createdAt = new Date();
+      course.createdBy = user._id;
+      return course.save();
     },
     deleteCourse(root, args, { user }) {
       if (!user) {
         throw new AuthenticationError(
-          'you must be logged in to delete a course'
-        )
+          "you must be logged in to delete a course"
+        );
       }
       // Course.del
-      console.log(args)
+      console.log(args);
 
-      return Course.deleteMany({ _id: { $in: args.ids } })
+      return Course.deleteMany({ _id: { $in: args.ids } });
 
       // return Course.deleteOne({ _id: args.id })
     },
     updateCourse(root, args, { user }) {
       if (!user) {
         throw new AuthenticationError(
-          'you must be logged in to update a course'
-        )
+          "you must be logged in to update a course"
+        );
       }
-      let _tempCource = Object.assign({}, args)
+      let validate = yup.object().shape({
+        name: yup.string().required(),
+        id: yup.string().required(),
 
-      delete _tempCource.id
-      return Course.updateOne({ _id: args.id }, { $set: _tempCource })
-    },
-  },
-}
+      });
+      await validate.validate(args).catch(function(err) {
+        throw new Error(err);
+      });
+      let _tempCource = Object.assign({}, args);
 
-export default resolvers
+      delete _tempCource.id;
+      return Course.updateOne({ _id: args.id }, { $set: _tempCource });
+    }
+  }
+};
+
+export default resolvers;
